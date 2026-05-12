5f671b8b7aa0cd57edc82fe1db305127439d844c
AmirmasoudCS <aammiirrrmasoud@gmail.com>
2026-05-12 19:46:25 +0330

    Adding the max_depth parameter to the formatter file.

--- tree_printer/formatter.py
+++ tree_printer/formatter.py
@@ -3,9 +3,15 @@ class TreeFormatter:
     def format(
             self,
             node : TreeNode,
-            prefix : str=""
+            prefix : str="",
+            depth : int = 0,
+            max_depth : int | None = None
     ) -> list[str]:
+        if max_depth is not None and depth > max_depth:
+            return []
         lines = []
+        if depth == 0 :
+            lines.append(node.name)
         for index, child in enumerate(node.children):
             is_last = index == len(node.children) - 1
             connector = "└── " if is_last else "├── "
@@ -15,7 +21,9 @@ class TreeFormatter:
                 lines.extend(
                     self.format(
                         child,
-                        prefix+extension
+                        prefix=prefix+extension,
+                        depth=depth+1,
+                        max_depth=max_depth
                     )
                 )
         return lines
