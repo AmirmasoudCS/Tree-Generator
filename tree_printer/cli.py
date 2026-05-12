771fb51f52e8eda9977f8b0b5eb888160af6e42e
AmirmasoudCS <aammiirrrmasoud@gmail.com>
2026-05-12 19:46:03 +0330

    Adding a command line to generate until a max_depth is reached.

--- tree_printer/cli.py
+++ tree_printer/cli.py
@@ -3,18 +3,24 @@ from .printer import  TreePrinter
 from .formatter import TreeFormatter
 def run():
     parser = argparse.ArgumentParser(
-        description="Print a directory tree"
+        description="========== Print a directory tree =========="
     )
     parser.add_argument(
         "path",
         nargs="?",
         default=".",
-        help="Root directory"
+        help="Root directory to generate the tree on"
+    )
+    parser.add_argument(
+        "--max-depth",
+        type=int,
+        default=None,
+        help="Maximum directory depth that will be generated"
     )
     args = parser.parse_args()
     printer = TreePrinter(root_path=args.path)
     formatter = TreeFormatter()
     tree = printer.build_tree()
-    lines = formatter.format(tree)
+    lines = formatter.format(tree, max_depth=args.max_depth)
     for line in lines:
         print(line)
