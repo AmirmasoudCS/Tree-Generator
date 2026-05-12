b4a1910377141b38f00c43fed146300bb250054f
AmirmasoudCS <aammiirrrmasoud@gmail.com>
2026-05-12 19:31:16 +0330

    Improving the main file to corespond with CLI file.

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-from tree_printer import TreePrinter, TreeFormatter
-def main():
-    printer = TreePrinter()
-    formatter = TreeFormatter()
-    tree = printer.build_tree()
-    lines = formatter.format(tree)
-    for line in lines:
-        print(line)
+from tree_printer.cli import run
 if __name__ == "__main__":
-    main()
+    run()
