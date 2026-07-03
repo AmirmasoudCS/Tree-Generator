9ed07f0ff96adf69aa2f3b89be733b30ebd1e61f
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-07-03 19:36:07 +0330

    Adding a function to test positional pathing.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,3 +6,8 @@ def test_default_path_is_dot():
     args = parser.parse_args([])
     assert args.path == "."
 
+def test_positional_path():
+    parser = build_parser()
+    args = parser.parse_args(["some/folder"])
+    assert args.path == "some/folder"
+
